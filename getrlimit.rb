56835b6c9289345b7e93b2ec82477bc06ba08e98
irasally <irasally@gmail.com>
2014-01-15 17:48:16 +0900

    ファイルディスクリプタ数以外のシステムリソースの制限を確認する
    第6章: プロセスにはリソースの制限がある

--- getrlimit.rb
+++ getrlimit.rb
@@ -1,2 +1,15 @@
+# -*- coding: utf-8 -*-
+# ファイルディスクリプタの最大数
 p Process.getrlimit(:NOFILE)
+
+# プロセスのユーザーが作成できる最大プロセス数
+p Process.getrlimit(:NPROC)
+
+# プロセスが作成できるファイルサイズの最大値
+p Process.getrlimit(:FSIZE)
+
+# プロセススタックの最大サイズ
+p Process.getrlimit(:STACK)
+
+# リソースに制限がないことを意味する定数の値
 p Process::RLIM_INFINITY
